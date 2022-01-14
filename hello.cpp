d57f574a43485c39933a6e3ceff1a662e90f0bcd
A-ZONE-LC <953882438@qq.com>
2022-01-14 16:53:15 +0800

    提交测试

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-14 16:52:22
+ * @LastEditTime: 2022-01-14 16:52:23
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \cpp-grammer-study\hello.cpp
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "hello VS Code" << endl;
+    return 0;
+}
